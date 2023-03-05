2b0f7e21899d161ca42806d67e26fc27e5e0a264
PatricioSouza <AntonioPatricio@gmail.com>
2023-03-05 10:10:00 -0300

    tudo ok

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+function App() {
+
+  const [dados, setDados] = useState()
+
+  const fetchData = async() => {
+    const response = await fetch("https://nadastestert.onrender.com/api/parties", {
+      method: "GET",
+      credentials: 'include'
+    })
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+    setDados(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <div className="App">
+      {dados && dados.map((dado) => (
+        <h1>{dado.title}</h1>
+      ))}
+    </div>
+  )
+}
+
+export default App
