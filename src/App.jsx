606e96a490101b8fdfe8af01aa1fbc82ee55c401
PatricioSouza <AntonioPatricio@gmail.com>
2023-03-08 09:40:05 -0300

    123

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [dados, setDados] = useState()
 
   const fetchData = async() => {
-    const response = await fetch("https://nadastestert.onrender.com/api/parties")
+    const response = await fetch("https://nadastestert.onrender.com/api/parties", {mode:'cors'})
     console.log(response)
     const data = await response.json()
     console.log(data)
